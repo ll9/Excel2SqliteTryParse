6aee161a5b0cbb982ba310128c241a841f5d7647
ll9 <llwierer@web.de>
2018-09-19 22:15:09 +0200

    Load Excel DataTable

--- Excel2Sqlite/ExcelHandler.cs
+++ Excel2Sqlite/ExcelHandler.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -14,6 +17,45 @@ namespace Excel2Sqlite
             var connection = new SQLiteConnection(connectionString);
             connection.Open();
             return connection;
+        }
+
+        private static DataTable LoadExcelDataTable(string path)
+        {
+            using (var workBook = new XLWorkbook(path))
+            {
+                using (var workSheet = workBook.Worksheet(1))
+                {
+                    DataTable dataTable = new DataTable();
+
+                    //Loop through the Worksheet rows.
+                    bool firstRow = true;
+                    foreach (IXLRow row in workSheet.Rows())
+                    {
+                        //Use the first row to add columns to DataTable.
+                        if (firstRow)
+                        {
+                            foreach (IXLCell cell in row.Cells())
+                            {
+                                dataTable.Columns.Add(cell.Value.ToString());
+                            }
+                            firstRow = false;
+                        }
+                        else
+                        {
+                            //Add rows to DataTable.
+                            dataTable.Rows.Add();
+                            int i = 0;
+
+                            foreach (IXLCell cell in row.Cells(row.FirstCellUsed().Address.ColumnNumber, row.LastCellUsed().Address.ColumnNumber))
+                            {
+                                dataTable.Rows[dataTable.Rows.Count - 1][i] = cell.Value.ToString();
+                                i++;
+                            }
+                        }
+                    }
+                    return dataTable;
+                }
+            }
         }
     }
 }
