840e5b93e2d24c071f85411f79436b4f79c0aa18
ll9 <llwierer@web.de>
2018-09-19 22:06:37 +0200

    Creating Db File

--- Excel2Sqlite/Form1.cs
+++ Excel2Sqlite/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +26,7 @@ namespace Excel2Sqlite
                 var dbPath = UserInteractionHandler.GetDialogResult<SaveFileDialog>("Sqlite (*.sqlite) | *.sqlite");
                 if (dbPath != null)
                 {
-                    MessageBox.Show("OK");
+                    File.Create(dbPath);
                 }
             }
         }
